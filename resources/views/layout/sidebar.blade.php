590fc55f3f7f1596c9bb5492480cf6ee9924f510
iqbalwaldan <iqbalwaldan.social@gmail.com>
2024-10-11 15:10:40 +0700

    Change icon sidebar

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -18,7 +18,7 @@
             </li>
             <li class="sidebar-item {{ Request::is('product*') ? 'active' : '' }}">
                 <a class="sidebar-link" href="/product">
-                    <i class="align-middle" data-feather="layers"></i> <span class="align-middle">Produk</span>
+                    <i class="align-middle" data-feather="package"></i> <span class="align-middle">Produk</span>
                 </a>
             </li>
             <li class="sidebar-item {{ Request::is('type*') ? 'active' : '' }}">
@@ -28,27 +28,27 @@
             </li>
             <li class="sidebar-item {{ Request::is('incoming*') ? 'active' : '' }}">
                 <a class="sidebar-link" href="/incoming">
-                    <i class="align-middle" data-feather="layers"></i> <span class="align-middle">Produk Masuk</span>
+                    <i class="align-middle" data-feather="arrow-down-left"></i> <span class="align-middle">Produk Masuk</span>
                 </a>
             </li>
             <li class="sidebar-item {{ Request::is('outgoing*') ? 'active' : '' }}">
                 <a class="sidebar-link" href="/outgoing">
-                    <i class="align-middle" data-feather="layers"></i> <span class="align-middle">Produk Keluar</span>
+                    <i class="align-middle" data-feather="arrow-up-right"></i> <span class="align-middle">Produk Keluar</span>
                 </a>
             </li>
             <li class="sidebar-item {{ Request::is('stock*') ? 'active' : '' }}">
                 <a class="sidebar-link" href="/stock">
-                    <i class="align-middle" data-feather="layers"></i> <span class="align-middle">Balance Stok</span>
+                    <i class="align-middle" data-feather="archive"></i> <span class="align-middle">Balance Stok</span>
                 </a>
             </li>
             <li class="sidebar-item {{ Request::is('report*') ? 'active' : '' }}">
                 <a class="sidebar-link" href="/report">
-                    <i class="align-middle" data-feather="layers"></i> <span class="align-middle">Report</span>
+                    <i class="align-middle" data-feather="file-text"></i> <span class="align-middle">Report</span>
                 </a>
             </li>
             <li class="sidebar-item {{ Request::is('cash-flow*') ? 'active' : '' }}">
                 <a class="sidebar-link" href="/cash-flow">
-                    <i class="align-middle" data-feather="layers"></i> <span class="align-middle">Keuangan</span>
+                    <i class="align-middle" data-feather="dollar-sign"></i> <span class="align-middle">Keuangan</span>
                 </a>
             </li>
         </ul>
