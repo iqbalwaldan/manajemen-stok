d3a8aa5b0ada9d79db558a9b4088d7127651d191
iqbalwaldan <iqbalwaldan.social@gmail.com>
2024-10-15 00:51:51 +0700

    Edit CashFlow

--- app/Http/Controllers/Client/CashFlowController.php
+++ app/Http/Controllers/Client/CashFlowController.php
@@ -3,23 +3,55 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use App\Models\ProductIncoming;
 use App\Models\ProductInstallment;
 use App\Models\ProductSalesReport;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Yajra\DataTables\Facades\DataTables;
 
 class CashFlowController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $income = ProductSalesReport::sum('total_profit');
-        $outcome = ProductIncoming::sum('total_price');
+        $totalDp = ProductIncoming::where('payment_type', 'installment')->sum('dp');
+        $totalPaidOff = ProductIncoming::where('payment_type', 'installment')->sum('paid_off');
+        $totalOutcomeInstallments = ProductIncoming::where('payment_type', 'installment')->sum('total_price');
+        $totalOutcome = ProductIncoming::sum('total_price');
+
+        $installments = ProductInstallment::with('productIncoming.product')
+            ->select('product_installments.datetime_payment', 'product_installments.product_incoming_id', 'product_installments.installment', 'products.name as product_name')
+            ->join('product_incomings', 'product_incomings.id', '=', 'product_installments.product_incoming_id')
+            ->join('products', 'products.id', '=', 'product_incomings.product_id')
+            ->orderBy('datetime_payment', 'DESC')
+            ->get();
+
+        if ($request->ajax()) {
+            return DataTables::of($installments)
+                ->addIndexColumn()
+                ->editColumn('datetime_payment', function ($installments) {
+                    return Carbon::parse($installments->datetime_payment)->format('d-m-Y');
+                })
+                ->editColumn('product_incoming_id', function ($installments) {
+                    return (string)$installments->product_incoming_id;
+                })
+                ->editColumn('product_name', function ($installments) {
+                    return (string)$installments->product_name;
+                })
+                ->editColumn('installment', function ($installments) {
+                    return (string)$installments->installment;
+                })
+                ->toJson();
+        };
 
         return view('admin.cash-flow.index', [
             'title' => 'Keuangan',
             'active' => 'cash-flow',
-            'income' => $income,
-            'outcome' => $outcome,
+            'totalDp' => $totalDp,
+            'totalPaidOff' => $totalPaidOff,
+            'totalOutcomeInstallments' => $totalOutcomeInstallments,
+            'totalOutcome' => $totalOutcome,
         ]);
     }
 
--- resources/views/admin/cash-flow/index.blade.php
+++ resources/views/admin/cash-flow/index.blade.php
@@ -12,12 +12,12 @@
             <div class="d-flex">
                 <div class="w-100">
                     <div class="row">
-                        <div class="col-sm-6">
+                        <div class="col-sm-3">
                             <div class="card">
                                 <div class="card-body">
                                     <div class="row">
                                         <div class="col mt-0">
-                                            <h5 class="card-title">Total Keuntungan</h5>
+                                            <h5 class="card-title">Total DP Terbayar</h5>
                                         </div>
                                         <div class="col-auto">
                                             <div class="statSuccess">
@@ -25,16 +25,16 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <h1 class="mt-1 mb-3">Rp{{ number_format($income, 0, ',', '.') }}</h1>
+                                    <h1 class="mt-1 mb-3">Rp{{ number_format($totalDp, 0, ',', '.') }}</h1>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-sm-6">
+                        <div class="col-sm-3">
                             <div class="card">
                                 <div class="card-body">
                                     <div class="row">
                                         <div class="col mt-0">
-                                            <h5 class="card-title">Total Pengeluaran</h5>
+                                            <h5 class="card-title">Total Sisa Pelunasan</h5>
                                         </div>
                                         <div class="col-auto">
                                             <div class="statDanger">
@@ -42,7 +42,41 @@
                                             </div>
                                         </div>
                                     </div>
-                                    <h1 class="mt-1 mb-3">Rp{{ number_format($outcome, 0, ',', '.') }}</h1>
+                                    <h1 class="mt-1 mb-3">Rp{{ number_format($totalPaidOff, 0, ',', '.') }}</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col mt-0">
+                                            <h5 class="card-title">Total Pengeluaran (Cicilan)</h5>
+                                        </div>
+                                        <div class="col-auto">
+                                            <div class="statDanger">
+                                                <i class="align-middle" data-feather="dollar-sign"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <h1 class="mt-1 mb-3">Rp{{ number_format($totalOutcomeInstallments, 0, ',', '.') }}</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col mt-0">
+                                            <h5 class="card-title">Total Pengeluaran (Cicilan + Cash)</h5>
+                                        </div>
+                                        <div class="col-auto">
+                                            <div class="statDanger">
+                                                <i class="align-middle" data-feather="dollar-sign"></i>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <h1 class="mt-1 mb-3">Rp{{ number_format($totalOutcome, 0, ',', '.') }}</h1>
                                 </div>
                             </div>
                         </div>
@@ -50,5 +84,72 @@
                 </div>
             </div>
         </div>
+        <div class="row">
+            <div class="col-12 d-flex">
+                <div class="card flex-fill">
+                    <div class="card-header">
+                        <h5 class="card-title mb-0">Tabel Cicilan</h5>
+                    </div>
+                    <div class="px-3">
+                        <table id="table-installments" class="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th style="width: 4%">No</th>
+                                    <th style="width: 24%">Tanggal Bayar</th>
+                                    <th style="width: 24%">Produk Masuk ID</th>
+                                    <th style="width: 24%">Nama Produk</th>
+                                    <th style="width: 24%">Nominal Bayar</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+@endsection
+@section('script')
+    <script>
+        $(document).ready(function() {
+            $('#table-installments').DataTable({
+                processing: true,
+                serverSide: true,
+                ajax: "/cash-flow",
+                columns: [{
+                        data: null,
+                        searchable: false,
+                        orderable: false,
+                        width: 100,
+                        render: function(data, type, row, meta) {
+                            return meta.row + meta.settings._iDisplayStart + 1;
+                        }
+                    },
+                    {
+                        data: 'datetime_payment',
+                        name: 'datetime_payment',
+                        searchable: true,
+                        orderable: true
+                    },
+                    {
+                        data: 'product_incoming_id',
+                        name: 'product_incoming_id',
+                        searchable: true,
+                        orderable: true
+                    },
+                    {
+                        data: 'product_name',
+                        name: 'product_name',
+                        searchable: true,
+                        orderable: true
+                    },
+                    {
+                        data: 'installment',
+                        name: 'installment',
+                        searchable: true,
+                        orderable: true
+                    },
+                ]
+            });
+        });
+    </script>
 @endsection
